b44be19a118fb5dcbc2309b1cdb2d6206f25efab
Ralphxdx <87979806+Ralphxdx@users.noreply.github.com>
2022-03-16 12:54:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const form1 = document.getElementById("myText");
+// console.log(form1.value);
+
+const butt = document.getElementById("name");
+butt.addEventListener('click', function() {
+    const dier = document.getElementById("myText")
+    console.log(dier.value);
+    
+});
